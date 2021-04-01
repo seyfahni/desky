42a5136b85f0a259632bf2f4ce6a2abbee938cf6
Niklas Seyfarth <niklas@seyfarth.de>
2021-04-01 16:49:42 +0200

    Extract GraphicAsset to own module.

--- desky/__main__.py
+++ desky/__main__.py
@@ -1,74 +1,9 @@
-import math
-import os
 import pathlib
 import collections
-import wx.svg
-from PIL import Image
+import wx
 from ruamel.yaml import YAML
 
-from desky import trigger
-
-
-def prepare_image(image_file):
-    file_without_extension, image_type = os.path.splitext(image_file)
-
-    if image_type.casefold() == ".svg":
-        svg_image = wx.svg.SVGimage.CreateFromFile(image_file)
-        image_file = file_without_extension + ".png"
-        width = math.ceil(svg_image.width)
-        height = math.ceil(svg_image.height)
-        png_image = svg_image.ConvertToBitmap(width=width, height=height)
-        png_image.SaveFile(image_file, wx.BITMAP_TYPE_PNG)
-
-    image = Image.open(image_file)
-    if "A" in image.getbands():
-        alpha_channel = image.getchannel("A")
-        region_mask = alpha_channel.point(lambda a: a != 0 and 255)
-        region_mask = region_mask.convert("1")
-    else:
-        region_mask = Image.new("1", image.size, 1)
-    region_mask_file = file_without_extension + "_region_mask.png"
-    region_mask.save(region_mask_file, "PNG")
-
-    return {
-        "image": image_file,
-        "region_mask": region_mask_file,
-    }
-
-
-Point = collections.namedtuple('Point', ['x', 'y'])
-
-
-class GraphicAsset:
-
-    def __init__(self, config) -> None:
-        super().__init__()
-        prepared_images = prepare_image(config['file'])
-        self.image = wx.Bitmap(prepared_images['image'])
-        region_mask = wx.Bitmap(prepared_images['region_mask'])
-        assert region_mask.Size == self.image.Size
-        self.size = region_mask.Size
-        offset_config = config.get('position', {'x': 0, 'y': 0})
-        self.offset = Point(offset_config.get('x', 0), offset_config.get('y', 0))
-        self.region = wx.Region(region_mask, wx.Colour(0, 0, 0))
-        box = self.region.GetBox()
-        self.move(-box.GetX(), -box.GetY())
-        self.region.Offset(self.offset.x, self.offset.y)
-        self.active = config.get('active', True)
-
-    def move(self, offset_x, offset_y):
-        self.region.Offset(offset_x, offset_y)
-        self.offset = Point(self.offset.x + offset_x, self.offset.y + offset_y)
-
-    def draw_active(self, context):
-        if self.active:
-            context.DrawBitmap(self.image, self.offset.x, self.offset.y, self.size.width, self.size.height)
-
-    def draw(self, context):
-        context.DrawBitmap(self.image, self.offset.x, self.offset.y, self.size.width, self.size.height)
-
-    def toggle_active(self):
-        self.active = not self.active
+from desky import trigger, asset
 
 
 class DeskyFrame(wx.Frame):
@@ -82,12 +17,12 @@ class DeskyFrame(wx.Frame):
         self.load_assets(config)
 
         full_region = wx.Region()
-        for asset in self.assets.values():
-            full_region.Union(asset.region)
+        for single_asset in self.assets.values():
+            full_region.Union(single_asset.region)
 
         box = full_region.GetBox()
-        for asset in self.assets.values():
-            asset.move(-box.GetX(), -box.GetY())
+        for single_asset in self.assets.values():
+            single_asset.move(-box.GetX(), -box.GetY())
         size = box.GetSize()
 
         style = wx.FRAME_NO_TASKBAR | wx.STAY_ON_TOP | wx.FRAME_SHAPED | wx.BORDER_NONE
@@ -108,7 +43,7 @@ class DeskyFrame(wx.Frame):
         self.assets.clear()
         assets_config = config['assets']
         for asset_id, asset_config in assets_config.items():
-            self.assets[asset_id] = GraphicAsset(asset_config)
+            self.assets[asset_id] = asset.GraphicAsset(asset_config)
 
     def on_paint(self, event):
         dc = wx.PaintDC(self)
@@ -129,9 +64,9 @@ class DeskyFrame(wx.Frame):
 
     def calculate_active_region(self):
         self.active_region.Clear()
-        for asset in self.assets.values():
-            if asset.active:
-                self.active_region.Union(asset.region)
+        for single_asset in self.assets.values():
+            if single_asset.active:
+                self.active_region.Union(single_asset.region)
         self.SetShape(self.active_region)
 
 
